c9c5ede3b167cca8de0fea26f79ade0b742ce327
Kobi <kobi.shasha@gmail.com>
2019-12-24 00:50:14 +0200

    Data Binding Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MySite2';
+export class AppComponent implements OnInit {
+
+  title = 'MySite';
+  public volume:number = 5;
+  public grade:number =50;
+
+
+
+  public constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  public buttonClicked(): void {
+  }
+
+  onUpdteName(event:Event){
+    console.log(event);
+    console.log((<HTMLInputElement>event.target).value);
+    
+  }
+
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { RedSquareComponent } from './components/red-square/red-square.component';
+import { GreenSquareComponent } from './components/green-square/green-square.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedSquareComponent,
+    GreenSquareComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
